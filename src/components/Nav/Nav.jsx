ff6e6b1b2f77ea46cd462bbea28e0049e42be0b0
Vivek Bhardwaj <vivek@Viveks-MacBook-Air.local>
2022-12-10 11:55:34 -0800

    add packages, update scaffolding

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import './nav.scss'
+
+function Nav() {
+  return ( 
+    <div className="nav">
+      <img src="/logo.png" alt="Gesundheit Logo"/>
+    </div>
+   );
+}
+
+export default Nav;
---
+++ src/components/PixelsContainer/PixelsContainer.jsx
@@ -0,0 +1,12 @@
+// styles
+import './pixels-container.scss';
+import DragSelect from 'dragselect';
+
+function PixelsContainer() {
+  return ( 
+    <div className="pixels-container">
+    </div>
+   );
+}
+
+export default PixelsContainer;
